f0c2e7bc8ff12e8ee832cf71120f01285e103e0a
dipesh arya <dipesharya28@gmail.com>
2024-10-24 14:33:52 +0530

    second commit

--- components/WSIViewer.jsx
+++ components/WSIViewer.jsx
@@ -124,13 +124,7 @@ const WSIViewer = () => {
             </table>
           </div>
 
-          {/* Add button */}
-          <div className="flex text-black space-x-2 cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>
-            <p>Add</p>
-          </div>
+          
         </div>
 
         {/* Center Panel: WSI Zoomed View */}
